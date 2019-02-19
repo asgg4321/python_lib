17714f1b590c58b88c0d9ef6fe53f471cc31cbba
wing <1124167773@qq.com>
2019-02-19 16:26:30 +0800

    提交人：Wing 提交备注：python练习代码整理到python_test文件夹

--- python_algorithms/interpolation_search.py
+++ python_algorithms/interpolation_search.py
@@ -12,4 +12,27 @@ def interpSearch(lst, x):  # 默认lst为均匀有序排布
     high = len(lst) - 1  # 默认定义为最大值下标
 
     while lst[low] < x < lst[high]:
-        mid = low +
+        mid = low + (x - lst[low])*(high-low)//(lst[high] - lst[low])  # 取比较接近于x的值为mid
+        if x < lst[mid]:
+            high = mid - 1
+        elif x > lst[mid]:
+            low = mid + 1
+        else:
+            return mid
+    if lst[low] == x:
+        return low
+    if lst[high] == x:
+        return high
+    return None
+
+print(interpSearch(items, 1)) # None
+print(interpSearch(items, 7))  # 3
+print(interpSearch(items, 19))
+
+# *** simplified speed test ***
+from datetime import datetime
+
+items = range[0, 1000000]
+count = 100
+
+
